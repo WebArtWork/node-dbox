fbeb17a0aabeb69ec6de3972c8fbcb7a4a64ef63
Boogie1989 <sergeybugai@gmail.com>
2016-05-08 23:49:56 +0300

    download

--- index.js
+++ index.js
@@ -1,29 +1,90 @@
 'use strict';
 var request = require("request");
 module.exports = function(config) {
-	var root = config.root || "/";
+    var root = config.root || "/";
 
-	return {
-		root: root,
-		client: function(accessToken) {
-			var accessToken = accessToken;
+    return {
+        root: root,
+        client: function(accessToken) {
+            var accessToken = accessToken;
 
-			return {
-				mkdir: function(path, cb) {
-					var args = {
-						"method": "POST",
-						"headers": {
-							"content-type": "application/json",
-							"Authorization": "Bearer "+accessToken
-						},
-						
-						"url": 'https://api.dropboxapi.com/2/files/create_folder',
-						"body": JSON.stringify({path: root+path})
-					}
-					return request(args, cb);
-				},
-			}
-		}
-	}
+            return {
+                mkdir: function(path, cb) {
+                    var args = {
+                        "method": "POST",
+                        "headers": {
+                            "content-type": "application/json",
+                            "Authorization": "Bearer " + accessToken
+                        },
+
+                        "url": 'https://api.dropboxapi.com/2/files/create_folder',
+                        "body": JSON.stringify({
+                            path: root + path
+                        })
+                    }
+                    return request(args, cb);
+                },
+
+                upload: function(path, body, cb) {
+                
+                    var args = {
+                        "method": "POST",
+                        "headers": {
+                            "content-type": "application/octet-stream",
+                            "Authorization": "Bearer " + accessToken,
+                            "Dropbox-API-Arg": JSON.stringify({
+                                path: root+path,
+                                mode: "add",
+                                autorename: true,
+                                mute: false
+                            }),
+                           
+                        },
+                         "body": body,
+                        "url": 'https://content.dropboxapi.com/2/files/upload',
+                    }
+                 
+                    return request(args, cb);
+                },
+
+
+
+                download: function(path, cb) {
+                    var args = {
+                        "method": "POST",
+                        "headers": {
+                            "Authorization": "Bearer " + accessToken,
+                            "Dropbox-API-Arg": JSON.stringify({
+                                path: root + path
+                            })
+                        },
+                        "url": 'https://content.dropboxapi.com/2/files/download',
+                    }
+                            return request(args, cb)
+                },
+
+
+                // account: function(path, cb) {
+                //     var args = {
+                //         "method": "POST",
+                //         "headers": {
+                //             "Authorization": "Bearer " + accessToken,
+                //             "Dropbox-API-Arg": JSON.stringify({
+                //                 path: root + path
+                //             })
+                //         },
+                //         "url": 'https://api.dropboxapi.com/2/users/get_account',
+                //     }
+                //             return request(args, cb)
+                // },
+
+
+
+
+
+
+            }
+        }
+    }
 
 }
--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 var should = require("should");
 var dbox = require("../index")({});
+var fs = require('fs');
+
 
 describe("all test start", function() {
 	var client;
@@ -7,18 +9,47 @@ describe("all test start", function() {
 	before(function(done) {
 		console.log('process.argv');
 		console.log(process.argv);
-		client = dbox.client('accessToken');
+		client = dbox.client('IlOMi7jJOjUAAAAAAAAAiNrZ8k-pdwVYadT3RA77SMkgy91zRCgKo3vK1jgAB71C');
 		done();
 	})
 
-	it("should create new folder", function(done) {
-		var newName = 'newFolder'+Date.now();
-		client.mkdir(newName, function(err, response, status) {
-			/*err.should.have.property(null);
-			status.name.should.have.property(newName);*/
-			done();
-		})
-	})
+	// it("should create new folder", function(done) {
+	// 	var newName = 'newFolder'+Date.now();
+	// 	client.mkdir(newName, function(err, response, status) {
+	// 		/*err.should.have.property(null);
+	// 		status.name.should.have.property(newName);*/
+	// 		done();
+	// 	})
+	// })
+  it("should get contents of file", function(done) {
+    client.download("111.jpg", function(status, reply){
+      // status.should.eql(200)
+
+      fs.writeFile('2222.jpg',reply.body,   (err) => {
+  if (err) throw err;
+  console.log('It\'s saved!');
+  done()
+});
+      
+    })
+  })
+
+
+
+//     it("should get contents of file", function(done) {
+// fs.readFile('test/222.jpg', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+
+//      client.upload("22222222.jpg", data, function(status, reply){
+//       // status.should.eql(200)
+//       var x = JSON.stringify(reply.body)
+//       console.log(reply.body);
+//       done()
+//     })
+// });
+ 
+//   })
 
 
 	after(function() {
@@ -26,3 +57,4 @@ describe("all test start", function() {
 	})
 
 })
+
